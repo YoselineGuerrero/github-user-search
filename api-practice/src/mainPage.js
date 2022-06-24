843bebcf256039e38098aa038677600344477b91
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-06-24 09:26:24 -0500

    footer fixed for mobile

--- api-practice/src/mainPage.js
+++ api-practice/src/mainPage.js
@@ -128,12 +128,18 @@ export default function MainPage(){
 
       <hr style={{marginTop:'40px', width:'90%'}}/>
       <div id='footer'>
-        <img src={GitHubLogo} width="35px"  alt=''></img>
-        <p id='footer-title'>© 2022 Yoseline Guerrero</p>
+      <div id='footer-img'>
+          <img src={GitHubLogo} width="35px"  alt=''></img>
+          <span id='footer-title'>© 2022 Yoseline Guerrero</span>
+        </div>
         <div id='footer-a'>
         {footer_links.map((link) => (
           <a id='footer-links' key={link.name} href={link.link} target='_blank' rel="noreferrer">{link.name}</a>
         ))}
+        </div>
+        <div id='footer-img-small'>
+          <img src={GitHubLogo} width="35px"  alt=''></img>
+          <span id='footer-title'>© 2022 Yoseline Guerrero</span>
         </div>
       </div>
     </div>
