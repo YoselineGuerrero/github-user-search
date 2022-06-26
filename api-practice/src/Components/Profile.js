73123b8e4e79259407ac3add9f26653a7776f30c
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-06-25 21:55:12 -0500

    icons added

--- api-practice/src/Components/Profile.js
+++ api-practice/src/Components/Profile.js
@@ -1,6 +1,11 @@
 import '../mainPage.css';
 import React, { useEffect, useState } from 'react';
 import { getUser } from './ApiCalls';
+import people from '../icons/people-icon.png';
+import building from '../icons/building-icon.png';
+import link from '../icons/link-icon.png';
+import map from '../icons/map-icon.png';
+import twitter from '../icons/twitter-icon.png';
 
 export default function Profile(username){
   const [user, setUser] = useState([]);
@@ -22,19 +27,31 @@ export default function Profile(username){
       <p id='username'>{user.login}</p>
       <p>{user.bio}</p>
       <div className='center-f'>
+        <img src={people} width="16" height="16" alt='people icon' style={user.login ? {} : { display: 'none' }}></img>
         <span className='number'>{user.followers}</span>
         <span className='subtitle' style={user.login ? {} : { display: 'none' }}> follower ·</span>
         <span className='number'>{user.following}</span>
         <span className='subtitle' style={user.login ? {} : { display: 'none' }}> following</span>
       </div>
-      <p className='user-info'>{user.company}</p>
-      <p className='user-info'>{user.location}</p>
-      <p className='user-info'><a href={user.blog} id='footer-links'  target='_blank' rel="noreferrer">{user.blog}</a></p>
-      <p className='user-info'>{user.twitter_username}</p>
-
+      <div className='center-f'>
+        <img src={building} width="16" height="16" alt='people icon' style={user.compnay ? {} : { display: 'none' }} className='padding5'></img>
+        <span className='user-info'>{user.company}</span>
+      </div>
+      <div className='center-f'>
+        <img src={map} width="16" height="16" alt='people icon' style={user.location ? {} : { display: 'none' }} className='padding5'></img>
+        <span className='user-info'>{user.location}</span>
+      </div>
+      <div className='center-f'>
+        <img src={link} width="16" height="16" alt='people icon' style={user.blog ? {} : { display: 'none' }} className='padding5'></img>
+        <span className='user-info'><a href={user.blog} id='footer-links' target='_blank' rel="noreferrer">{user.blog}</a></span>
+      </div>
+      <div className='center-f'>
+        <img src={twitter} width="16" height="16" alt='people icon' style={user.twitter ? {} : { display: 'none' }} className='padding5'></img>
+        <span className='user-info'>{user.twitter_username}</span>
+      </div>
       <p className='user-info'>{user.email}</p>
-      <p className='user-info'>{user.created_at}</p>
-      <p className='user-info'>{user.updated_at}</p>
+      <p className='user-info' style={user.login ? {} : { display: 'none' }}>Created: {user.created_at}</p>
+      <p className='user-info' style={user.login ? {} : { display: 'none' }}>Updated: {user.updated_at}</p>
 
       <div className='button-box'>
         <a href={user.html_url} target="_blank" rel="noreferrer">
