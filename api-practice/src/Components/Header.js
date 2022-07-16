3a393f7fda1acbb8d6b48e10d95b6bf5634509a9
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-15 19:40:04 -0500

    Started dark mode.

--- api-practice/src/Components/Header.js
+++ api-practice/src/Components/Header.js
@@ -1,8 +1,27 @@
 import '../mainPage.css';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import GitHubLogo from '../GitHub-logo-green.png';
 
 export default function Header(){
+  const ref = useRef(null);
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+    else {
+      document.documentElement.setAttribute('data-theme', 'light');   
+      localStorage.setItem('theme', 'light');
+    }    
+  }
+
+  useEffect(() => {
+    ref.current.addEventListener('change', switchTheme, false);
+  }
+  ,
+  [ref]);
+
   return(
     <div id='navbar'>
       <a href='/'>
@@ -10,7 +29,7 @@ export default function Header(){
       </a>
       <p id='navbar-title'> GitHub User Search</p>
       <label className="switch">
-        <input type="checkbox"></input>
+        <input type="checkbox" ref={ref}></input>
         <span className="slider round"></span>
       </label>
     </div>
