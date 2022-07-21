31babd7f01aec085862c69fc5a5e716101679c61
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-21 13:25:49 -0500

    relocated router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './mainPage';
 import ErrorPage from './Components/error404';
+import { BrowserRouter  } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="*" element={<ErrorPage/>}/>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
