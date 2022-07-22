3ed37bdc3db2a62ce21f48dc8187e4f2f1be0e34
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-22 17:45:11 -0500

    images to svg on profile and repos files

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,6 @@
 import '../mainPage.css';
 import React, { useEffect, useState } from 'react';
 import { getUser } from './ApiCalls';
-import people from '../icons/people-icon.png';
-import building from '../icons/building-icon.png';
-import link from '../icons/link-icon.png';
-import map from '../icons/map-icon.png';
-import twitter from '../icons/twitter-icon.png';
 
 export default function Profile(username){
   const [user, setUser] = useState([]);
@@ -29,31 +24,41 @@ export default function Profile(username){
           <p id='username' className='start-content'>{user.login}</p>
           <p className='text-color start-content'>{user.bio}</p>
           <div className='center-f'>
-            <img src={people} width="16" height="16" alt='people icon' style={user.login ? {} : { display: 'none' }}></img>
+            <svg style={user.login ? {} : { display: 'none' }} height="16" width='16' className='fill-color'>
+              <path d="M 5.5 3.5 a 2 2 0 1 0 0 4 a 2 2 0 0 0 0 -4 Z M 2 5.5 a 3.5 3.5 0 1 1 5.898 2.549 a 5.507 5.507 0 0 1 3.034 4.084 a 0.75 0.75 0 1 1 -1.482 0.235 a 4.001 4.001 0 0 0 -7.9 0 a 0.75 0.75 0 0 1 -1.482 -0.236 A 5.507 5.507 0 0 1 3.102 8.05 A 3.49 3.49 0 0 1 2 5.5 Z M 11 4 a 0.75 0.75 0 1 0 0 1.5 a 1.5 1.5 0 0 1 0.666 2.844 a 0.75 0.75 0 0 0 -0.416 0.672 v 0.352 a 0.75 0.75 0 0 0 0.574 0.73 c 1.2 0.289 2.162 1.2 2.522 2.372 a 0.75 0.75 0 1 0 1.434 -0.44 a 5.01 5.01 0 0 0 -2.56 -3.012 A 3 3 0 0 0 11 4 Z"/>
+            </svg>
             <span className='number'>{user.followers}</span>
             <span className='subtitle' style={user.login ? {} : { display: 'none' }}> follower ·</span>
             <span className='number'>{user.following}</span>
             <span className='subtitle' style={user.login ? {} : { display: 'none' }}> following</span>
           </div>
           <div className='center-f'>
-            <img src={building} width="16" height="16" alt='people icon' style={user.compnay ? {} : { display: 'none' }} className='padding5'></img>
+            <svg style={user.company === null || user.company === ''|| user.company === undefined ? { display: 'none'} : {}} height="16" width='16' className='fill-color'>
+              <path d="M 1.5 14.25 c 0 0.138 0.112 0.25 0.25 0.25 H 4 v -1.25 a 0.75 0.75 0 0 1 0.75 -0.75 h 2.5 a 0.75 0.75 0 0 1 0.75 0.75 v 1.25 h 2.25 a 0.25 0.25 0 0 0 0.25 -0.25 V 1.75 a 0.25 0.25 0 0 0 -0.25 -0.25 h -8.5 a 0.25 0.25 0 0 0 -0.25 0.25 v 12.5 Z M 1.75 16 A 1.75 1.75 0 0 1 0 14.25 V 1.75 C 0 0.784 0.784 0 1.75 0 h 8.5 C 11.216 0 12 0.784 12 1.75 v 12.5 c 0 0.085 -0.006 0.168 -0.018 0.25 h 2.268 a 0.25 0.25 0 0 0 0.25 -0.25 V 8.285 a 0.25 0.25 0 0 0 -0.111 -0.208 l -1.055 -0.703 a 0.75 0.75 0 1 1 0.832 -1.248 l 1.055 0.703 c 0.487 0.325 0.779 0.871 0.779 1.456 v 5.965 A 1.75 1.75 0 0 1 14.25 16 h -3.5 a 0.75 0.75 0 0 1 -0.197 -0.026 c -0.099 0.017 -0.2 0.026 -0.303 0.026 h -3 a 0.75 0.75 0 0 1 -0.75 -0.75 V 14 h -1 v 1.25 a 0.75 0.75 0 0 1 -0.75 0.75 h -3 Z M 3 3.75 A 0.75 0.75 0 0 1 3.75 3 h 0.5 a 0.75 0.75 0 0 1 0 1.5 h -0.5 A 0.75 0.75 0 0 1 3 3.75 Z M 3.75 6 a 0.75 0.75 0 0 0 0 1.5 h 0.5 a 0.75 0.75 0 0 0 0 -1.5 h -0.5 Z M 3 9.75 A 0.75 0.75 0 0 1 3.75 9 h 0.5 a 0.75 0.75 0 0 1 0 1.5 h -0.5 A 0.75 0.75 0 0 1 3 9.75 Z M 7.75 9 a 0.75 0.75 0 0 0 0 1.5 h 0.5 a 0.75 0.75 0 0 0 0 -1.5 h -0.5 Z M 7 6.75 A 0.75 0.75 0 0 1 7.75 6 h 0.5 a 0.75 0.75 0 0 1 0 1.5 h -0.5 A 0.75 0.75 0 0 1 7 6.75 Z M 7.75 3 a 0.75 0.75 0 0 0 0 1.5 h 0.5 a 0.75 0.75 0 0 0 0 -1.5 h -0.5 Z"/>
+            </svg>
             <span className='user-info'>{user.company}</span>
           </div>
           <div className='center-f'>
-            <img src={map} width="16" height="16" alt='people icon' style={user.location ? {} : { display: 'none' }} className='padding5'></img>
+            <svg style={user.location === null || user.location === ''|| user.location === undefined ? { display: 'none'} : {}} height="16" width='16' className='fill-color padding5'>
+              <path d="M 11.536 3.464 a 5 5 0 0 1 0 7.072 L 8 14.07 l -3.536 -3.535 a 5 5 0 1 1 7.072 -7.072 v 0.001 Z m 1.06 8.132 a 6.5 6.5 0 1 0 -9.192 0 l 3.535 3.536 a 1.5 1.5 0 0 0 2.122 0 l 3.535 -3.536 Z M 8 9 a 2 2 0 1 0 0 -4 a 2 2 0 0 0 0 4 Z"/>
+            </svg>
             <span className='user-info'>{user.location}</span>
           </div>
           <div className='center-f'>
-            <img src={link} width="16" height="16" alt='people icon' style={user.blog ? {} : { display: 'none' }} className='padding5'></img>
+            <svg style={user.blog === null || user.blog === '' || user.blog === undefined ? { display: 'none'} : {}} height="16" width='16' className='fill-color padding5'>
+              <path d="M 7.775 3.275 a 0.75 0.75 0 0 0 1.06 1.06 l 1.25 -1.25 a 2 2 0 1 1 2.83 2.83 l -2.5 2.5 a 2 2 0 0 1 -2.83 0 a 0.75 0.75 0 0 0 -1.06 1.06 a 3.5 3.5 0 0 0 4.95 0 l 2.5 -2.5 a 3.5 3.5 0 0 0 -4.95 -4.95 l -1.25 1.25 Z m -4.69 9.64 a 2 2 0 0 1 0 -2.83 l 2.5 -2.5 a 2 2 0 0 1 2.83 0 a 0.75 0.75 0 0 0 1.06 -1.06 a 3.5 3.5 0 0 0 -4.95 0 l -2.5 2.5 a 3.5 3.5 0 0 0 4.95 4.95 l 1.25 -1.25 a 0.75 0.75 0 0 0 -1.06 -1.06 l -1.25 1.25 a 2 2 0 0 1 -2.83 0 Z"/>
+            </svg>
             <span className='user-info'><a href={user.blog} id='footer-links' target='_blank' rel="noreferrer">{user.blog}</a></span>
           </div>
           <div className='center-f'>
-            <img src={twitter} width="16" height="16" alt='people icon' style={user.twitter ? {} : { display: 'none' }} className='padding5'></img>
+            <svg style={user.twitter_username === null || user.twitter_username === ''|| user.twitter_username === undefined ? { display: 'none'} : {}} height="16" width='16' viewBox="0 0 273.5 222.3" className='fill-color padding5'>
+              <path d="M 273.5 26.3 a 109.77 109.77 0 0 1 -32.2 8.8 a 56.07 56.07 0 0 0 24.7 -31 a 113.39 113.39 0 0 1 -35.7 13.6 a 56.1 56.1 0 0 0 -97 38.4 a 54 54 0 0 0 1.5 12.8 A 159.68 159.68 0 0 1 19.1 10.3 a 56.12 56.12 0 0 0 17.4 74.9 a 56.06 56.06 0 0 1 -25.4 -7 v 0.7 a 56.11 56.11 0 0 0 45 55 a 55.65 55.65 0 0 1 -14.8 2 a 62.39 62.39 0 0 1 -10.6 -1 a 56.24 56.24 0 0 0 52.4 39 a 112.87 112.87 0 0 1 -69.7 24 a 119 119 0 0 1 -13.4 -0.8 a 158.83 158.83 0 0 0 86 25.2 c 103.2 0 159.6 -85.5 159.6 -159.6 c 0 -2.4 -0.1 -4.9 -0.2 -7.3 a 114.25 114.25 0 0 0 28.1 -29.1"/>
+            </svg>
             <span className='user-info'>{user.twitter_username}</span>
           </div>
           <p className='user-info'><a id='footer-links' href={`mailto:`+user.email} target="_blank" rel="noreferrer">{user.email}</a></p>
-          <p className='user-info' style={user.login ? {} : { display: 'none' }}>Created: {user.created_at}</p>
-          <p className='user-info' style={user.login ? {} : { display: 'none' }}>Updated: {user.updated_at}</p>
+          <p className='user-info' style={user.login ? {} : { display: 'none' }}>Created: {user.created_at?.substring(0, 10)}</p>
+          <p className='user-info' style={user.login ? {} : { display: 'none' }}>Updated: {user.updated_at?.substring(0, 10)}</p>
         </div>
       </div>
       <div className='button-box user-button'>
--- src/Components/Repos.js
+++ src/Components/Repos.js
@@ -17,7 +17,6 @@ export default function Repos(user){
 
   return(
     <div style={repos.length > 0 ? {} : { display: 'none' }}>
-      <hr/>
       <h2 className='text-color'>Repos</h2>
       <div className="grid-container-repo">
         {repos.map((repo) => (
@@ -28,16 +27,32 @@ export default function Repos(user){
               <p className='padding10'>Updated: {repo.updated_at.substring(0, 10)}</p>
               <p className='padding10'>Pushed: {repo.pushed_at.substring(0, 10)}</p>
             </div>
-            <div className='subtitle'>
-              <p>Stars: {repo.stargazers_count}</p>
-              <p>Watchers: {repo.watchers_count}</p>
-              <p>Issues Open: {repo.open_issues}</p>
+            <div className='subtitle' style={{justifyContent: 'space-around'}}>
+              <div className='inline-center'>
+                <svg height="16" width='16' className='fill-color padding5'>
+                  <path d="M 8 0.25 a 0.75 0.75 0 0 1 0.673 0.418 l 1.882 3.815 l 4.21 0.612 a 0.75 0.75 0 0 1 0.416 1.279 l -3.046 2.97 l 0.719 4.192 a 0.75 0.75 0 0 1 -1.088 0.791 L 8 12.347 l -3.766 1.98 a 0.75 0.75 0 0 1 -1.088 -0.79 l 0.72 -4.194 L 0.818 6.374 a 0.75 0.75 0 0 1 0.416 -1.28 l 4.21 -0.611 L 7.327 0.668 A 0.75 0.75 0 0 1 8 0.25 Z m 0 2.445 L 6.615 5.5 a 0.75 0.75 0 0 1 -0.564 0.41 l -3.097 0.45 l 2.24 2.184 a 0.75 0.75 0 0 1 0.216 0.664 l -0.528 3.084 l 2.769 -1.456 a 0.75 0.75 0 0 1 0.698 0 l 2.77 1.456 l -0.53 -3.084 a 0.75 0.75 0 0 1 0.216 -0.664 l 2.24 -2.183 l -3.096 -0.45 a 0.75 0.75 0 0 1 -0.564 -0.41 L 8 2.694 v 0.001 Z"/>
+                </svg>
+                <p>Stars: {repo.stargazers_count}</p>
+              </div>
+              <div className='inline-center'>
+                <svg height="16" width='16' className='fill-color padding5'>
+                  <path d="M 1.679 7.932 c 0.412 -0.621 1.242 -1.75 2.366 -2.717 C 5.175 4.242 6.527 3.5 8 3.5 c 1.473 0 2.824 0.742 3.955 1.715 c 1.124 0.967 1.954 2.096 2.366 2.717 a 0.119 0.119 0 0 1 0 0.136 c -0.412 0.621 -1.242 1.75 -2.366 2.717 C 10.825 11.758 9.473 12.5 8 12.5 c -1.473 0 -2.824 -0.742 -3.955 -1.715 C 2.92 9.818 2.09 8.69 1.679 8.068 a 0.119 0.119 0 0 1 0 -0.136 Z M 8 2 c -1.981 0 -3.67 0.992 -4.933 2.078 C 1.797 5.169 0.88 6.423 0.43 7.1 a 1.619 1.619 0 0 0 0 1.798 c 0.45 0.678 1.367 1.932 2.637 3.024 C 4.329 13.008 6.019 14 8 14 c 1.981 0 3.67 -0.992 4.933 -2.078 c 1.27 -1.091 2.187 -2.345 2.637 -3.023 a 1.619 1.619 0 0 0 0 -1.798 c -0.45 -0.678 -1.367 -1.932 -2.637 -3.023 C 11.671 2.992 9.981 2 8 2 Z m 0 8 a 2 2 0 1 0 0 -4 a 2 2 0 0 0 0 4 Z"/>
+                </svg>
+                <p>Watchers: {repo.watchers_count}</p>
+              </div>
+              <div className='inline-center'>
+                <svg height="16" width='16' className='fill-color padding5'>
+                  <path d="M8 9.5 a 1.5 1.5 0 1 0 0 -3 a 1.5 1.5 0 0 0 0 3 Z"/>
+                  <path d="M8 0 a 8 8 0 1 0 0 16 A 8 8 0 0 0 8 0 Z M 1.5 8 a 6.5 6.5 0 1 1 13 0 a 6.5 6.5 0 0 1 -13 0 Z"/>
+                </svg>
+                <p>Issues Open: {repo.open_issues}</p>
+              </div>
             </div>
             <div className='button-box'>
               <span className={repo.language} id='dot'></span>
               <span id='name'>{repo.language}</span>
             </div>
-            <p className='sub-color'>Description: {repo.description}</p>
+            <p className='sub-color'>{repo.description}</p>
             <div id="repo-tag">
             {repo.topics.map((topic) => (
               <span className='tag' key={topic}><b>{topic}</b></span>
