9bcddcf4e6d0db1a1a88fe7a6b2803b09e69776e
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-22 15:12:03 -0500

    intro and 404 style clean up

--- src/Components/error404.js
+++ src/Components/error404.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function ErrorPage() {
   return(
-    <div>
-      <p>Oops! Seems like that page doesn't exist...</p>
-      <a href='/' className='center-error'>
-        <button className='button-repo'>Click me to go to the main page</button>
-      </a>
+    <div id='container'>
+      <Header/>
+      <div style={{marginTop:'20px'}}>
+        <p className='center-text'>Oops! Seems like that page doesn't exist...</p>
+        <a href='/' className='center-error'>
+          <button className='button-repo'>Click me to go to the main page</button>
+        </a>
+      </div>
+      <Footer/>
     </div>      
   )
 } 
